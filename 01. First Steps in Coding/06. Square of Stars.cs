443fb2a584aacf2ebb1a2b398855cc95e8269d93
NikolaHristov98 <Rv^4OgJq>
2018-01-26 14:40:27 +0200

    Uploading the solutions to the exercises

---
+++ 01. First Steps in Coding/06. Square of Stars.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            string top_bottom = "", left_right = "";
+
+            for (int i = 0; i < size; i++)
+            {
+                top_bottom = top_bottom + "*";
+            }
+
+            if (size > 0)
+            {
+                left_right = "*";
+                for (int i = 0; i < size - 2; i++)
+                {
+                    left_right = left_right + " ";
+                }
+                left_right = left_right + "*";
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0 || i == size - 1) Console.WriteLine(top_bottom);
+                else Console.WriteLine(left_right);
+            }
+
+
+        }
+    }
+}
